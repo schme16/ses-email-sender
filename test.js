8d1ec25bb596f60448ada2bb3ba9445ea151ab97
Shane Gadsby <schme16@gmail.com>
2016-02-07 12:29:43 +1000

    unfreezing the modules (-npm-shrinkwrap.json) added nodemon @ 2.2.x (aka beta channel; adds ical support) added ics-creator; allows you to  send `meeting requests AKA: ical Events`
    Signed-off-by: Shane Gadsby <schme16@gmail.com>

--- test.js
+++ test.js
@@ -1,14 +1,32 @@
 
 //Initialize the library (only required once)
 var email = require('./index.js')({
-	AWSAccessKeyID: process.env.AWSAccessKeyID,
-	AWSSecretKey: process.env.AWSSecretKey
-})
+		AWSAccessKeyID: process.env.AWSAccessKeyID,
+		AWSSecretKey: process.env.AWSSecretKey
+	}),
+	ics = require('ics-creator'),
+	ical = ics.createICS({
+		'organizerName':'Shazza',
+		'organizerEmail':'schme16@gmail.com',
+		'attendeeName':'Shane Gadsby',
+		'attendeeEmail':'shane.gadsby@usq.edu.au',
+		'body':'This is gonna be delish!',
+		'subject':'Want to get Maccas for dinner tonight?',
+		'location':'Maccas',
+		'start': new Date(),
+		'end': new Date(),
+		'currentTime': new Date()
+	})
 
 //How to send an email
 email({
 	from: "schme16@gmail.com", // sender address
-	to: "schme16@gmail.com", // list of receivers, can be semicolon seperated
+	to: "shane.gadsby@usq.edu.au", // list of receivers, can be semicolon seperated
 	subject: "Lets eat at Maccas tonight", // Subject line
-	html: "Hey Shane, lets go eat at MacDonalds tonight!" // HTML message to send
+	html: "Hey Shane, lets go eat at MacDonalds tonight!", // HTML message to send
+	icalEvent: {
+		method: 'request',
+		content: ical
+	}
+
 })
